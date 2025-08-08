657c14a463ec380d5d57ec2a84ba2fe933a6c5c8
nikzarch <razerr468@gmail.com>
2025-08-08 12:30:43 +0300

    refactoring

--- backend/cmd/igropoisk/igropoisk.go
+++ backend/cmd/igropoisk/igropoisk.go
@@ -3,11 +3,17 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"io"
+	"log"
 	"os"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
 	r := gin.New()
 	f, _ := os.Create("log.txt")
 	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
